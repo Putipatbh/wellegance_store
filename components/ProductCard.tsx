19ee40bdeacda25aa0883fc1eeab7c5a6a5e311b
Putipatbh <putipat.s@ku.th>
2024-11-02 14:19:18 +0700

    padding

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -12,7 +12,7 @@ interface ProductCardProps {
 const ProductCard = ({ product, updateSignedInUser }: ProductCardProps ) =>  {
 
     return (
-        <Link href={`/products/${product._id}`} className="w-[220px] flex flex-col gap-2 rounded-lg bg-white-1 hover:shadow-xl">
+        <Link href={`/products/${product._id}`} className="w-[220px] flex flex-col gap-2 rounded-lg bg-white-1 p-3 hover:shadow-xl">
             <Image
                 src={product.media[0]}
                 alt="product"
